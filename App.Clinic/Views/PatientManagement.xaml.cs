d77eb50d0f2e8e186b862b4be62146fd7d437b11
Chris Mills <crmills@fsu.edu>
2024-09-11 10:06:35 -0400

    Added patient list view for patient management. Closes #3

--- App.Clinic/Views/PatientManagement.xaml.cs
+++ App.Clinic/Views/PatientManagement.xaml.cs
@@ -1,10 +1,21 @@
+using Library.Clinic.Models;
+using Library.Clinic.Services;
+
 namespace App.Clinic.Views;
 
 public partial class PatientManagement : ContentPage
 {
+	public List<Patient> Patients
+	{
+		get
+		{
+			return PatientServiceProxy.Current.Patients;
+		}
+	}
 	public PatientManagement()
 	{
 		InitializeComponent();
+		BindingContext = this;
 	}
 
     private void CancelClicked(object sender, EventArgs e)
