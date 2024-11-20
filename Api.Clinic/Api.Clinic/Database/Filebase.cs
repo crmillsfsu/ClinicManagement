3940fdaf307f2045951b0b08a1e4f28cf753263b
Chris Mills <crmills@fsu.edu>
2024-11-20 09:48:26 -0500

    Added Filebase

---
+++ Api.Clinic/Api.Clinic/Database/Filebase.cs
@@ -0,0 +1,106 @@
+﻿using Library.Clinic.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.ToDoApplication.Persistence
+{
+    public class Filebase
+    {
+        private string _root;
+        private string _patientRoot;
+        private Filebase _instance;
+
+
+        public Filebase Current
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new Filebase();
+                }
+
+                return _instance;
+            }
+        }
+
+        private Filebase()
+        {
+            _root = @"C:\temp";
+            _patientRoot = $"{_root}\\Patients";
+        }
+
+        public int LastKey
+        {
+            get
+            {
+                if (Patients.Any())
+                {
+                    return Patients.Select(x => x.Id).Max();
+                }
+                return 0;
+            }
+        }
+
+        public Patient AddOrUpdate(Patient patient)
+        {
+            //set up a new Id if one doesn't already exist
+            if(patient.Id <= 0)
+            {
+                patient.Id = LastKey + 1;
+            }
+
+            //go to the right place
+            string path = $"{_patientRoot}\\{patient.Id}.json";
+            
+
+            //if the item has been previously persisted
+            if(File.Exists(path))
+            {
+                //blow it up
+                File.Delete(path);
+            }
+
+            //write the file
+            File.WriteAllText(path, JsonConvert.SerializeObject(patient));
+
+            //return the item, which now has an id
+            return patient;
+        }
+        
+        public List<Patient> Patients
+        {
+            get
+            {
+                var root = new DirectoryInfo(_patientRoot);
+                var _patients = new List<Patient>();
+                foreach(var patientFile in root.GetFiles())
+                {
+                    var patient = JsonConvert
+                        .DeserializeObject<Patient>
+                        (File.ReadAllText(patientFile.FullName));
+                    if(patient != null)
+                    {
+                        _patients.Add(patient);
+                    }
+
+                }
+                return _patients;
+            }
+        }
+
+
+        public bool Delete(string type, string id)
+        {
+            //TODO: refer to AddOrUpdate for an idea of how you can implement this.
+            return true;
+        }
+    }
+
+
+   
+}
